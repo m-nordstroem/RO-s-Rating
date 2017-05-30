7300f43f57e9f5449c41da0c7c0c60060a046bc4
Stefan Korfitz Jønsson <stefan@korfitz.com>
2017-05-30 10:30:34 +0200

    localhost mongodb connection-string added to config.js

--- config.js
+++ config.js
@@ -1,6 +1,11 @@
 module.exports = {
 	'port': process.env.PORT || 8080,
-	//'database': 'mongodb://node:noder@novus.modulusmongo.net:27017/Iganiq8o',
-	'database': 'mongodb://admin:12345@ds119810.mlab.com:19810/michdb',	
-	'secret': 'ilovescotchscotchyscotchscotch'
+
+	// Connection-string for : LOCALHOST
+    // 'database': 'mongodb://localhost:27017/Ros-Rating-DB',
+
+    // Connection-string for : mLAB
+    'database': 'mongodb://admin:12345@ds119810.mlab.com:19810/michdb',
+
+    'secret': 'ilovescotchscotchyscotchscotch'
 };
